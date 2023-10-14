943b9451a53e710dd2389b1e462ca4396ff9f35f
David <tumbata85@gmail.com>
2023-10-14 13:11:49 +0300

    Spring setup

---
+++ src/main/java/com/example/team8forum/Team8ForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.team8forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8ForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team8forum/Team8ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team8forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team8ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
