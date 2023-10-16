7a6dad4007cf8d87dc2c27c4c5031e2fac54c7f9
David <tumbata85@gmail.com>
2023-10-16 16:17:47 +0300

    Updated database design with admin phones directing to user

--- db/createDb.sql
+++ db/createDb.sql
@@ -1,14 +1,3 @@
-create table admins
-(
-    admin_id   int auto_increment
-        primary key,
-    first_name varchar(60) not null,
-    last_name  varchar(60) not null,
-    email      varchar(70) not null,
-    constraint admins_pk2
-        unique (email)
-);
-
 create table phones
 (
     phone_id     int         not null
@@ -18,31 +7,31 @@ create table phones
         unique (phone_number)
 );
 
-create table admins_phones
-(
-    admin_id int null,
-    phone_id int null,
-    constraint admins_phones_admins_admin_id_fk
-        foreign key (admin_id) references admins (admin_id),
-    constraint admins_phones_phones_phone_id_fk
-        foreign key (phone_id) references phones (phone_id)
-);
-
 create table users
 (
-    user_id      int auto_increment
+    user_id    int auto_increment
         primary key,
-    first_name   varchar(50)          not null,
-    last_name    varchar(60)          not null,
-    username     varchar(60)          not null,
-    password     varchar(50)          not null,
-    email        varchar(70)          not null,
-    is_moderator tinyint(1)           not null,
-    is_blocked   tinyint(1) default 0 not null,
+    first_name varchar(50)          not null,
+    last_name  varchar(60)          not null,
+    username   varchar(60)          not null,
+    password   varchar(50)          not null,
+    email      varchar(70)          not null,
+    is_admin   tinyint(1)           not null,
+    is_blocked tinyint(1) default 0 not null,
     constraint users_pk2
         unique (email),
     constraint users_pk3
         unique (username)
+);
+
+create table admin_phones
+(
+    user_id  int null,
+    phone_id int null,
+    constraint admin_phones_phones_phone_id_fk
+        foreign key (phone_id) references phones (phone_id),
+    constraint admin_phones_users_user_id_fk
+        foreign key (user_id) references users (user_id)
 );
 
 create table posts
@@ -80,7 +69,3 @@ create table likes
     constraint likes_user___fk
         foreign key (user_id) references users (user_id)
 );
-
-
-
-
