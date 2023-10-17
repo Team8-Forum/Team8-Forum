4f764bd51be4007672851a9ad66accd54a965d1b
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-10-17 15:51:26 +0300

    Adding getters and setters for likes and id fields, also new hasCode and equals methods.

--- src/main/java/com/example/team8forum/models/Post.java
+++ src/main/java/com/example/team8forum/models/Post.java
@@ -63,16 +63,32 @@ public class Post {
         this.content = content;
     }
 
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return Objects.equals(createdBy, post.createdBy) && Objects.equals(title, post.title) && Objects.equals(content, post.content);
+        return id == post.id && likes == post.likes && Objects.equals(createdBy, post.createdBy) && Objects.equals(title, post.title) && Objects.equals(content, post.content);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(createdBy, title, content);
+        return Objects.hash(id, createdBy, title, content, likes);
     }
 }
