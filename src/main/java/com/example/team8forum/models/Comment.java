21b06804338c7748cf2073b8bf34ecdb664b6809
KatyaStepanova <kateryna.stepanova99@gmail.com>
2023-10-17 14:05:56 +0300

    Added Comment class and CommentDto class

--- src/main/java/com/example/team8forum/models/Comment.java
+++ src/main/java/com/example/team8forum/models/Comment.java
@@ -1,4 +1,63 @@
 package com.example.team8forum.models;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "post_id")
+    private int postId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    public Comment() {
+    }
+
+    public Comment(String comment, int postId, int userId) {
+        this.comment = comment;
+        this.postId = postId;
+        this.userId = userId;
+    }
+
+    public int getCommentId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
 }
+
---
+++ src/main/java/com/example/team8forum/models/CommentDto.java
@@ -0,0 +1,25 @@
+package com.example.team8forum.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CommentDto {
+        @NotNull
+        private int postId;
+        @NotBlank(message = "You may not leave a blank comment.")
+        private String content;
+
+        public CommentDto() {
+        }
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+
+    }
+
