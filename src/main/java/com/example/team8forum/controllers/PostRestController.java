a2aa4df42e7e1ac4319a99542e484b38b566e8f1
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-10-18 17:14:35 +0300

    Pushing AuthenticationHelper

--- src/main/java/com/example/team8forum/controllers/PostRestController.java
+++ src/main/java/com/example/team8forum/controllers/PostRestController.java
@@ -1,4 +1,14 @@
 package com.example.team8forum.controllers;
 
+import com.example.team8forum.helpers.PostMapper;
+import com.example.team8forum.services.contracts.PostService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostRestController {
+    private final PostService service;
+
+    private final PostMapper postMapper;
 }
